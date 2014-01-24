16d85e9e78ccaec3873ce1d9c0ab2be7406c6cc2
powerblogservice <powerblogservice@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-24 13:44:49 +0000

    Tagging CaptionPix 1.3.2 - fix bug when loading under AJAX
    git-svn-id: https://plugins.svn.wordpress.org/captionpix/tags/1.3.2@844531 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/plugin.php
+++ classes/plugin.php
@@ -1,25 +1,28 @@
 <?php
-$dir = dirname(__FILE__) . '/';
-require_once($dir . 'options.php');
-require_once($dir . 'updater.php');
-require_once($dir . 'theme-factory.php');
-require_once($dir . 'public.php');
-if (is_admin()) {
-	require_once($dir . 'admin.php');
-	require_once($dir . 'licence.php');
-	require_once($dir . 'defaults.php');
-	require_once($dir . 'themes.php');
-}
 
 class CaptionPixPlugin {
 	public static function init() {
+		$dir = dirname(__FILE__) . '/';
+		require_once($dir . 'options.php');
+		require_once($dir . 'updater.php');
+		require_once($dir . 'theme-factory.php');
+		require_once($dir . 'public.php');
 		CaptionPix::init();
-		if (is_admin()) {
-			CaptionPixAdmin::init();	
-			CaptionPixLicence::init();	
-			CaptionPixDefaults::init();			
-			CaptionPixThemes::init();	
-		}
+	}
+
+	public static function admin_init() {
+		$dir = dirname(__FILE__) . '/';
+		require_once($dir . 'admin.php');
+		require_once($dir . 'licence.php');
+		require_once($dir . 'defaults.php');
+		require_once($dir . 'themes.php');
+		CaptionPixAdmin::init();	
+		CaptionPixLicence::init();	
+		CaptionPixDefaults::init();			
+		CaptionPixThemes::init();	
 	}
 }
+
+add_action ('init',  array('CaptionPixPlugin', 'init'),0);
+if (is_admin()) add_action ('init',  array('CaptionPixPlugin', 'admin_init'),0);
 ?>
